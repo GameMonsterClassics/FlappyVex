a23650cca7f749c7a9a65869197590663a468c2a
Vespine <142816007+VespineSpring@users.noreply.github.com>
2024-09-17 15:54:51 +0530

    Some how works a little bit.

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -4,10 +4,16 @@ var game_running: bool = false
 var player_touches_ground: bool = false
 @onready var player: CharacterBody2D = $"../Player"
 
+var player_scene = preload("res://scenes/player.tscn")
+
 
 func _process(delta: float) -> void:
-	start_game()
 	player_dead()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept") and not game_running:
+		start_game()
 
 
 func reset() -> void:
@@ -17,7 +23,7 @@ func reset() -> void:
 
 func start_game() -> void:
 	if not player:
-		player.queue_redraw()
+		respawn_player()
 	
 	if game_running:
 		return
@@ -27,9 +33,16 @@ func start_game() -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		print("Game has been started.")
 		game_running = true
-		player.flap()
+		#player.flap()
+
 
 func player_dead() -> void:
 	if not player:
 		print("Player is dead.")
 		game_running = false
+
+
+func respawn_player() -> void:
+	player = player_scene.instantiate()
+	get_tree().current_scene.add_child(player)
+	player.position = Vector2(128, 256)
