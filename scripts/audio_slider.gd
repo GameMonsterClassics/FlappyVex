7129d87095f02a1fbaa9cae124b5cb2f11ccf9e6
Vespine <142816007+VespineSpring@users.noreply.github.com>
2024-10-13 11:54:28 +0530

    Added a mute toggle to volume sliders.

--- scripts/audio_slider.gd
+++ scripts/audio_slider.gd
@@ -1,7 +1,8 @@
 extends Control
 
 @onready var label: Label = $VBoxContainer/Label
-@onready var h_slider: HSlider = $VBoxContainer/HSlider
+@onready var h_slider: HSlider = $VBoxContainer/HBoxContainer/HSlider
+@onready var check_box: CheckBox = $VBoxContainer/HBoxContainer/CheckBox
 
 @export var bus_name: String
 
@@ -12,7 +13,23 @@ func _ready() -> void:
 	bus_index = AudioServer.get_bus_index(bus_name)
 	label.text = str(bus_name) + " Volume"
 	h_slider.value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
+	
+	if AudioServer.is_bus_mute(bus_index):
+		check_box.button_pressed = false
+		h_slider.editable = false
+	else:
+		check_box.button_pressed = true
+		h_slider.editable = true
 
 
 func _on_h_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+
+
+func _on_check_box_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		AudioServer.set_bus_mute(bus_index, false)
+		h_slider.editable = true
+	else:
+		AudioServer.set_bus_mute(bus_index, true)
+		h_slider.editable = false
