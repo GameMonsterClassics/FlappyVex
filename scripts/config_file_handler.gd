55c2a4cc37dd11abb45cd343422aeb6a972add06
Vespine <142816007+VespineSpring@users.noreply.github.com>
2024-10-13 19:16:52 +0530

    Finally Fixed the BGM Player Bug

--- scripts/config_file_handler.gd
+++ scripts/config_file_handler.gd
@@ -25,11 +25,16 @@ func _ready() -> void:
 
 func load_settings():
 	var video_settings = load_video_settings()
+	var volume_mute_settings = load_volume_mute_settings()
 	
 	if video_settings.fullscreen:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	
+	for bus_name in volume_mute_settings:
+		var bus_index = AudioServer.get_bus_index(bus_name)
+		AudioServer.set_bus_mute(bus_index, not volume_mute_settings[bus_name])
 
 
 func save_volume_settings(key: String, value):
