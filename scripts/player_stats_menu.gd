519179e7cc0c3ad582df9dc0d67c02c2f6c782da
Vespine <142816007+VespineSpring@users.noreply.github.com>
2024-10-16 22:42:43 +0530

    Completed all Menus

--- scripts/player_stats_menu.gd
+++ scripts/player_stats_menu.gd
@@ -1,5 +1,9 @@
 extends Control
 
 
-func _on_button_pressed() -> void:
+func _on_quit_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_credits_button_pressed() -> void:
+	pass # Replace with function body.
